895f221b6dd3864a6e0ddf879e21c1ec66d44848
Bishow08 <gdghamal522@gmail.com>
2024-04-14 17:33:04 +0545

    adding new question

--- javascript.js
+++ javascript.js
@@ -1,1 +1,6 @@
 //cloning , adding, committing and push
+write a implicit js function that formats the number in follwoing format
+
+git add .
+git commit -m "adding new question"
+git push origin main
