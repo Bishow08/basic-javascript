b6ce5105a923b22da223fa0a8350acdbd3964844
Bishow08 <gdghamal522@gmail.com>
2024-04-14 21:35:25 +0545

    keywords concept

---
+++ index.js
@@ -0,0 +1,20 @@
+//clone , add, commit and push
+
+// git add .
+// git commit -m "adding new question"
+// git push origin main
+
+// keywords
+// var= used for global variable define
+// let = reused variables
+// const = used for constant value
+
+var name1 = "bishal";
+var name1 ="hey"
+console.log(name);  // it will print honey because of the scope of let keyword
+
+const person ="John"
+const person1 ="Jane"
+
+console.log({person});
+console.log({person1});
