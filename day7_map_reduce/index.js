c29c631ae6f1351bcf72ae2248238d342189c0ea
Bishow08 <gdghamal522@gmail.com>
2024-04-18 16:02:30 +0545

    map filter reduce practice

---
+++ day7_map_reduce/index.js
@@ -0,0 +1,124 @@
+console.log("day 7");
+
+
+//revise
+
+
+//for => for(start, condition, stepper){}
+// do while do{//logic}while(condition)
+// while
+
+for(let i=0; i<=10;i++){
+    console.log(i);
+}
+
+let j=0;
+do {
+    console.log(j);
+    j++;
+}while(j<=10);
+
+let k=0;
+while( k<=10) {
+    console.log(k);
+    k++;
+}
+
+//statements
+//if(){}else{}
+//switch case
+
+
+const username ="raktim"
+const password ="raktim" 
+if(username === password){ // if use == it will return true or false but comparision only value  but === condidional compares both value and data types
+    console.log(true);
+} else {
+    console.log("Invalid User");
+}
+
+//switch case
+
+const day = 2;
+switch(day){
+    case 1:
+        console.log("Sunday");
+        break;
+
+        case 2:
+            console.log("Monday");
+            break;
+
+        case 3:
+                console.log("Tuesday");
+                break;
+
+        default:
+            console.log("invalid data");
+}
+
+// if else (es5)
+// es6
+//called as ternary Operator
+username === password ? console.log("loggen in"): console.log("Invalid");
+
+// Logical Operator
+
+//And && ===> 
+if(username==="raktim" && password==="raktim"){
+    console.log("Logged In");
+}
+
+//Not ====> 
+if(!username==="raktim"){
+    console.log("Valid user");
+}
+
+//Or || ===>  
+if(username==="raktim"|| password==="raktim"){
+    console.log("valid user");
+
+}
+
+//function
+
+// es6
+//Write a js function to add 2 numbers
+// result 2,2 ==>4
+const sum = (num1,num2)=>{ //num1 and num2 called as parameters
+return num1+num2;
+}
+//function call
+const result =sum(2,2); // when  we call the function 2,2 are called as arguments
+console.log({result});
+
+//es5
+function sums(num1,num2){
+    return num1+num2;
+}
+const results = sum(2,2);
+console.log({results});
+
+//Bubble sort logic
+//sort method with function comparison
+
+const people =[
+    {name: "raktim", age: 20},
+    {nme: "sita", age:10},
+    {name: "John", age :30}
+    
+];
+//sort in Ascending order by age => smalll to big
+
+// loop inside array
+//compare 1st number age to 2nd number age
+//swap the object if 1st is smaller than 2nd
+
+const myLogic = (a,b) =>{
+    return a.age-b.age; //
+}
+// sort logic >0 swap, <0 no swap  ,=0 no swap
+const resultSort = people.sort(myLogic);
+console.log({resultSort});
+
+
---
+++ day7_map_reduce_practice/index.js
@@ -0,0 +1,97 @@
+console.log("hello day 7 practice");
+
+//praciie array ===> map ,filter, sort, reduce,
+const characters = [
+    {
+        name: 'Luke Skywalker',
+        height: '172',
+        mass: '77',
+        eye_color: 'blue',
+        gender: 'male',
+    },
+    {
+        name: 'Darth Vader',
+        height: '202',
+        mass: '136',
+        eye_color: 'yellow',
+        gender: 'male',
+    },
+    {
+        name: 'Leia Organa',
+        height: '150',
+        mass: '49',
+        eye_color: 'brown',
+        gender: 'female',
+    },
+    {
+        name: 'Anakin Skywalker',
+        height: '188',
+        mass: '84',
+        eye_color: 'blue',
+        gender: 'male',
+    },
+];
+
+// const allNames = characters.map((character)=> character.name);
+// console.log({allNames});
+
+// MAP
+
+    // 1 Get an array of all names
+
+    const allNames = characters.map( (name)=> { return name.name}); // if function start with culey braces it should have 'return' keyword
+    console.log(allNames);
+
+    // 2 Get an array of all heights
+
+    const allHeights = characters.map((height)=> height.height);
+    console.log(allHeights);
+
+    // 3 Get an array of objects with just name and height properties
+
+    const nameHeight = characters.map((nameH)=>({ //Since an arrow function without curly braces expects a single expression, if you want to return an object literal directly from that expression, you must wrap it in parentheses.  
+        name:nameH.name,
+        height:nameH.height
+    }));
+    console.log(nameHeight);
+
+
+    // 4 Get an array of all first names
+
+    const firstName = characters.map((firstName)=> {return firstName.name.split(" ")[0]});
+    console.log(firstName);
+
+    // FILTER
+
+    // 1 Get characters with mass greater than 100
+    const massGreater = characters.filter((mass)=>parseInt(mass.mass)>100);
+    console.log(massGreater);
+
+    // 2 Get characters with height less than 200
+    const heightLess = characters.filter((heightless)=>  parseInt(heightless.height)<200)
+    console.log(heightLess);
+
+    // 3 Get all male characters
+    const male = characters.filter((male)=>male.gender==='male');
+    console.log(male);
+
+    // 4 Get all female characters
+    const female = characters.filter((female)=>female.gender==='female');
+    console.log(female);
+
+    // SORT
+
+    // 1 Sort by name
+
+
+    // 2 Sort by mass
+
+
+    // 3 Sort by height
+
+
+    // 4 Sort by gender
+ 
+
+
+
